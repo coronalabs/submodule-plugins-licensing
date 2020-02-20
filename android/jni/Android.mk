b8adb8cb87b0f1023b1b898288c0f01ebaa399ee
Vlad Shcherban <vlad@coronalabs.com>
2020-02-20 18:38:05 +0800

    Changing license to MIT

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,5 +1,11 @@
-# Copyright (C) 2012 Corona Labs Inc.
+#----------------------------------------------------------------------------
 #
+# This file is part of the Corona game engine.
+# For overview and more information on licensing please refer to README.md 
+# Home page: https://github.com/coronalabs/corona
+# Contact: support@coronalabs.com
+#
+#----------------------------------------------------------------------------
 
 # TARGET_PLATFORM := android-14
 
